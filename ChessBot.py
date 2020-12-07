886cfbadf7327f8c626ddc375d7d97a51dede6f2
Nick <nvanalfen2@gmail.com>
2020-12-07 18:24:22 -0500

    Create class for ChessBot

---
+++ ChessBot.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  7 18:21:46 2020
+
+@author: nvana
+"""
+
+import numpy as np
+import pandas as pd
+
+class ChessBot:
+    def __init__(self):
+        pass
